e852f5982c45e1d7a641eafebfbe98577d165dc3
prakhar mishra <107488385+prakharmishra263139@users.noreply.github.com>
2024-06-16 20:35:01 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (46.04%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        
+        long sumFormed=0;
+        long expectedSum=1;
+        int min=0;
+
+        int i=0;
+        int m = nums.length;
+
+        while(sumFormed<n){
+            if(sumFormed>=expectedSum){
+                expectedSum=sumFormed+1;
+            }else{
+                if(i<m && nums[i]<=expectedSum){
+                    sumFormed += nums[i];
+                    i++;
+                }else{
+                    min++;
+                    sumFormed+=expectedSum;
+                }
+            }
+        }
+        return min;
+    }
+}
