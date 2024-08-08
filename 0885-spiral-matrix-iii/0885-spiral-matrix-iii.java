0caa292df1c6b3936d00bacd0e950b75b216a33a
prakhar mishra <107488385+prakharmishra263139@users.noreply.github.com>
2024-08-08 23:03:24 +0530

    Time: 4 ms (81.31%), Space: 45.1 MB (57.24%) - LeetHub

---
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int directions[][] = {{0,1},{1,0},{0,-1},{-1,0}};
+        int n = rows*cols;
+        int ans[][]= new int[n][2];
+
+        ans[0][0]=rStart;
+        ans[0][1]=cStart;
+
+        int count=1;
+
+        int step=1;
+        int index=0;
+        while(count<n){
+            for(int times=0;times<2;times++){
+                int dr = directions[index%4][0];
+                int dc = directions[index%4][1];
+
+                for(int i=0;i<step;i++){
+                    rStart+=dr;
+                    cStart+=dc;
+
+                    if(rStart>=0 && rStart<rows && cStart>=0 && cStart<cols){
+                        ans[count][0]=rStart;
+                        ans[count][1]=cStart;
+                        count++;
+                    }
+                }
+
+                index++;
+            }
+            step++;
+        }
+
+        return ans;
+
+
+    }
+}
