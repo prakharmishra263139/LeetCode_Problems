0bd2ad671614a203727b428cf487596b033cf72d
prakhar mishra <107488385+prakharmishra263139@users.noreply.github.com>
2024-10-12 23:16:13 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (65.69%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -18,7 +18,6 @@ class Solution {
         if(root == null){
             return 0;
         }
-        
         if(root.left !=null && root.left.left == null && root.left.right ==null){
             return root.left.val + sumOfLeftLeaves(root.right);
     }
