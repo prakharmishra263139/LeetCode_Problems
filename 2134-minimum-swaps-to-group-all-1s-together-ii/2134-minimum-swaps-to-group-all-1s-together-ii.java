2b3d5b1205e65cf2701dc58041ac6772be9d6f54
prakhar mishra <107488385+prakharmishra263139@users.noreply.github.com>
2024-11-09 22:15:37 +0530

    Time: 7 ms (56.25%), Space: 59.3 MB (46.11%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
@@ -4,25 +4,20 @@ class Solution {
         for(int num:nums){
             windowSize +=num;
         }
-
         int zero=0;
-
         for(int i=0;i<windowSize;i++){
             if(nums[i]==0){
                zero++; 
             }
         }
-
         int min=zero;
         int start=0;
         int end=windowSize-1;
         int n=nums.length;
-
         while(start<n){
             if(nums[start]==0){
                 zero--;
             }
-
             start++;
             end++;
             if(nums[end%n]==0){
@@ -30,7 +25,6 @@ class Solution {
             }
             min = Math.min(min,zero);
         }
-
-        return min;
+    return min;
     }
 }
